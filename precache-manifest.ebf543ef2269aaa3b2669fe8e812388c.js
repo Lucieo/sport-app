2b0d40ebec8865091356eefbdac97e5594787731
aTabarly <luciecamilleory@gmail.com>
2019-06-02 22:46:45 +0200

    Updates

---
+++ precache-manifest.ebf543ef2269aaa3b2669fe8e812388c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e94a6b608cdf4f3d7ee5",
+    "url": "/sport-app/static/js/runtime~main.ed59b52a.js"
+  },
+  {
+    "revision": "e75413a3ea10688711f8",
+    "url": "/sport-app/static/js/main.ddcd34f6.chunk.js"
+  },
+  {
+    "revision": "23ba71e3a480b240ee1e",
+    "url": "/sport-app/static/js/2.a4723b52.chunk.js"
+  },
+  {
+    "revision": "e75413a3ea10688711f8",
+    "url": "/sport-app/static/css/main.df39015b.chunk.css"
+  },
+  {
+    "revision": "4fa057aeca6b92b52084535c1c1f4863",
+    "url": "/sport-app/index.html"
+  }
+];
